9baffe4cad1a5905fa7c9932572abc6b3d4fb3b0
David Parada <dfdo.parada@gmail.com>
2025-01-30 20:56:25 -0500

    quiz

--- prueba.R
+++ prueba.R
@@ -23,5 +23,16 @@ returns <- prices |>
   select(date, ret) |>
   drop_na()
 
+quantile_5 <- quantile(returns |> pull(ret) * 100, probs = 0.05)
+
 returns |>
   mutate(ret = ret * 100) |>
+  ggplot(aes(x = ret)) +
+  geom_histogram(bins = 100) +
+  geom_vline(aes(xintercept = quantile_5),
+                  linetype = "dashed") +
+  labs(
+    x = NULL,
+    y = NULL,
+    title = "mi otra distribucion"
+  )
